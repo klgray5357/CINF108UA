438a8f8170bc3b17ad0f5b908d66975a5c3f0067
Kelsey Gray <klgray@albany.edu>
2025-08-26 21:41:23 -0400

    Add files via upload

---
+++ CINF108 ASSIGNMENT1.py
@@ -0,0 +1,16 @@
+# Print Syllabus:
+def print_syllabus():
+    """
+    Print the syllabus rules for grading and assignments for the course.
+    """
+    # Grades
+    print("This course is A-E graded and is based off of graded assignments.")
+    print("Midterm and Final count for 20% of the final grade.")
+    print("If an assignment is late it will not be graded.")
+    print("If Dr. De Tuya is not able to read the code you wrote there will be a deduction in points for your grade.")
+    print("Your final grade will be on a scale of 100 points.")
+    # Assignments
+    print("Assignments will not have any makeup opportunities.")
+    print("Incomplete assignments will receive a deduction in points.")
+    print("Weekly assignments count for 80% of the final grade.")
+    print("A new assignment will be posted weekly on Brightspace.")
